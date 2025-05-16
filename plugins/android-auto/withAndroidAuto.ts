c8888c3086850f05cd3a2dab8bf8a1c4b64e7e5c
KMalkowski <krzysztof.malkowski098@gmail.com>
2025-05-16 16:50:48 +0200

    kotlin src

--- plugins/android-auto/withAndroidAuto.ts
+++ plugins/android-auto/withAndroidAuto.ts
@@ -13,6 +13,7 @@ const {
 const withAndroidAuto = (config: ExpoConfig) => {
   config = manifestModifier(config);
   config = withAndroidAutoDependency(config);
+  config = withAndroidSourceFiles(config);
   return config;
 };
 
@@ -53,7 +54,8 @@ const manifestModifier = (config: ExpoConfig) => {
     if (!hasService) {
       const serviceNode = {
         $: {
-          "android:name": "com.anonymous.expoandroidautohelloworld",
+          "android:name":
+            "com.anonymous.expoandroidautohelloworld.CarAppService",
           "android:exported":
             "true" as configPlugins.AndroidConfig.Manifest.StringBoolean,
         },
@@ -106,6 +108,54 @@ const withAndroidAutoDependency = (config: ExpoConfig) => {
       return props;
     },
   ]);
+};
+
+const withAndroidSourceFiles = (config: ExpoConfig) => {
+  const packageName = config.android?.package;
+
+  config = withDangerousMod(config, [
+    "android",
+    async (props) => {
+      const projectRoot = props.modRequest.projectRoot;
+      const sourceDir = path.join(projectRoot, "plugins/android-auto/src");
+      const targetDir = path.join(
+        projectRoot,
+        "android",
+        "app",
+        "src",
+        "main",
+        "java",
+        ...(packageName?.split(".") ?? []),
+      );
+
+      await fs.mkdir(targetDir, { recursive: true });
+      await fs.cp(sourceDir, targetDir, {
+        recursive: true,
+        force: true,
+      });
+
+      const xmlDir = path.join(
+        projectRoot,
+        "android",
+        "app",
+        "src",
+        "main",
+        "res",
+        "xml",
+      );
+
+      await fs.mkdir(xmlDir, { recursive: true });
+      await fs.cp(
+        path.join(projectRoot, "plugins/android-auto/automotive_app_desc.xml"),
+        path.join(xmlDir, "automotive_app_desc.xml"),
+        { force: true },
+      );
+
+      return props;
+    },
+  ]);
+
+  return config;
 };
 
 export default createRunOncePlugin(withAndroidAuto, "withAndroidAuto");
